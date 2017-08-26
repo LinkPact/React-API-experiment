c0e1670a57af657a12916c1358f843bcc72642dc
Jakob Willforss <jakob.willforss@hotmail.com>
2017-08-26 21:57:59 +0200

    Create basic Android native index file

---
+++ index.android.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+import StackNavigator from './App';
+
+AppRegistry.registerComponent('APIExample', () => StackNavigator);
