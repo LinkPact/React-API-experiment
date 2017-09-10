4da558bede73b96fbba3c27717391b17db1eda28
Martin <martinbaste@gmail.com>
2017-09-10 10:30:47 +0200

    add calendar component

--- src/components/screens/CalendarScreen.js
+++ src/components/screens/CalendarScreen.js
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View, Navigator, Button } from 'react-native';
 import {
 	StackNavigator,
 } from 'react-navigation';
-
+import { Calendar } from 'react-native-calendars';
 
 export default class CalendarScreen extends React.Component {
   static navigationOptions = {
@@ -14,7 +14,15 @@ export default class CalendarScreen extends React.Component {
     	return (
     		<View style={{flex: 1}}>
     			<Text>Group Calendar</Text>
-    			<Text>Group Calendar</Text>
+    			<Calendar 
+				   markedDates={{
+				    '2017-09-01': [{startingDay: true, color: 'orange'}],
+				    '2017-09-02': [{marked: true, color: 'orange'}],
+     				 '2017-09-03': [{endingDay: true, color: 'orange'}],
+				     '2017-09-04': [{startingDay: true, color: 'green'}, {endingDay: true, color: 'green'}]}}
+				     markingType={'interactive'}
+
+    			/>
 	     		<Button
 		        	title="Create new habit"
 		        	//onPress={() =>
