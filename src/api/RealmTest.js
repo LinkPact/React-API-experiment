e6f1dc8c042acaeb1ad82ec9e12b6cb61e3174a2
Jakob Willforss <jakob.willforss@hotmail.com>
2017-10-01 08:44:05 +0200

    Attempt to setup admin database - not running properly

--- src/api/RealmTest.js
+++ src/api/RealmTest.js
@@ -3,13 +3,64 @@
 // import realm from "./realm";
 import Realm from "realm";
 
-import { HabitEntry, Habit, Calendar } from "./realm";
+import { HabitEntry, Habit, Calendar, UserIDs } from "./realm";
 
 
 export function initRealmTest() {
 
     console.log("Running initRealmTest");
 
+
+    const REALM_SERVER = 'http://10.0.3.2:9080';
+
+    globalRealmSetup(REALM_SERVER, 'jakob.willforss@hotmail.com', 'ahbrJGZS2w8W');
+
+
+}
+
+function userLoginSync(server, user, pass) {
+    Realm.Sync.User.login(server, 'jakob.willforss@immun.lth.se', 'realm', (error, user) => {
+
+        console.log("Attempting login!");
+        console.log("User: " + user);
+        console.log("Error: " +  error);
+
+        if (!error) {
+            console.log("No error!");
+
+            Realm.open({
+                sync: {
+                    user: user,
+                    url: 'realm://object-server-url:9080/global_realm',
+                },
+                schema: [HabitEntry, Habit, Calendar]
+            }).then(realm => {
+                // return callback(null, realm);
+                return realm;
+            });
+
+            // Realm.open({
+            //     sync: {
+            //         user: user,
+            //         url: 'realm://object-server-url:9080/~/my-realm',
+            //     },
+            //     schema: [HabitEntry, Habit, Calendar]
+            // }).then(realm => {
+            //     // return callback(null, realm);
+            //     return realm;
+            // });
+        }
+        else {
+            // console.log("Error: " + new Error(error.message));
+            return new Error(error.message);
+            // return callback(new Error(error.message));
+        }
+
+    });
+}
+
+
+function writeHabitEntry() {
     realm.write(() => {
         realm.create('Habit', {
             name: 'Honda',
@@ -41,123 +92,55 @@ export function initRealmTest() {
         });
     });
 
-    // const habits = realm.objects('Habit');
+    const habits = realm.objects('Habit');
 
-    // console.log("--- Number of habits: " + habits.length + " ---");
-
-    Realm.Sync.User.login('http://10.0.3.2:9080', 'jakob.willforss@immun.lth.se', 'realm', (error, user) => {
-
-        console.log("Attempting login!");
-        console.log("User: " + user);
-        console.log("Error: " +  error);
-
-        if (!error) {
-            console.log("No error!");
-            Realm.open({
-                sync: {
-                    user: user,
-                    url: 'realm://object-server-url:9080/~/my-realm',
-                },
-                schema: [HabitEntry, Habit, Calendar]
-            }).then(realm => {
-
-                // return callback(null, realm);
-                return realm;
-            });
-        }
-        else {
-            // console.log("Error: " + new Error(error.message));
-            return new Error(error.message);
-            // return callback(new Error(error.message));
-        }
-
-    });
-
-    // Realm.Sync.User.login('http://10.0.3.2:9080', 'jakob.willforss@immun.lth.se', 'realm', (error, user) => {
-    //
-    //     if (error) console.log(error.message);
-    //
-    //     console.log(user);
-    //
-    // });
-
-    // Realm.Sync.User.register("http://127.0.0.1:9080", "jakob.willforss@test.com", "testpass", (error, user) => {
-    //
-    //     console.log("Registering test");
-    //
-    //     if (error) console.log(error);
-    //
-    //     console.log(user);
-    //
-    // });
-
-
-
-    // let user = Realm.Sync.User.current;
-    // if (user) {
-    //     let synchedRealm = new Realm({
-    //         sync: {
-    //             user: user,
-    //             url: 'realm://1.2.3.4:9080/~/my-realm',
-    //         },
-    //         schema: [Model1Schema, Model2Schema]
-    //     });
-    //     return synchedRealm;
-    // }
-
-// otherwise continue with login flow:
-
-    // Realm.Sync.User.login('http://1.2.3.4:9080', 'test@email.com', 'password', (error, user) => {
-//...
-
-
-
-
-    // Realm.Sync.User.registerWithProvider('http://127.0.0.1:9080', 'facebook', accessToken, (error, user) => {
-    //     if (error) console.log(error);
-    //
-    //     console.log(user);
-    //
-    //     const TestSchema = {
-    //         name: 'Test',
-    //         properties: {
-    //             name: 'string',
-    //             email: 'string'
-    //         }
-    //     };
-    //     const realm = new Realm({
-    //         sync: {
-    //             user: user,
-    //             url: 'realm://127.0.0.1:9080/~/REALM'
-    //         },
-    //         schema: [TestSchema]
-    //     });
-    //     realm.write(() => {
-    //         let test = realm.create('Test', {
-    //             "name": "test",
-    //             "email": "test@test.com"
-    //         })
-    //     });
-    //     const obj = realm.object("Test");
-    //     console.log(obj);
-    // });
+    console.log("--- Number of habits: " + habits.length + " ---");
 }
 
-// Realm.Sync.User[action](config.auth_uri, username, password,
-//     (error, user) => {
-//         if (error) {
-//             return callback(new Error(error.message));
-//         } else {
-//             realm = new Realm({
-//                 schema: [Task, TaskList],
-//                 sync: {
-//                     user,
-//                     url: config.db_uri
-//                 },
-//                 path: config.db_path
-//             });
-//             return callback(null, realm); // TODO errors
-//         }
-//     }
-// );
+
+function globalRealmSetup (realm_server, admin_email, admin_pass) {
+
+    console.log("globalRealmSetup");
+
+    Realm.Sync.User.login(realm_server, admin_email, admin_pass,
+        (error, adminUser) => {
+            if (!error) {
+
+                console.log("Admin user synced without errors");
+
+                let realm = new Realm({
+                    sync: {
+                        user: adminUser,
+                        url: 'realm://object-server-url:9080/globalRealm',
+                    },
+                    schema: [UserIDs]
+                });
+
+                // Apply permissions to everyone
+                const managementRealm = user.openManagementRealm();
+                console.log("Management realm: " + managementRealm);
+
+                let permObj;
+                managementRealm.write(() => {
+                    permObj = managementRealm.create('PermissionChange', {
+                        id: 1,   // implement something that creates a unique id.
+                        // id: generateUniqueId(),   // implement something that creates a unique id.
+                        createdAt: new Date(),
+                        updatedAt: new Date(),
+                        userId: '*', // To apply the permission changes for all Users authorized with the Object Server, specify a userId value of *.
+                        realmUrl: 'realm://object-server-url:9080/globalRealm',
+                    });
+                });
+
+                // Listen for `PermissionChange` object to be processed
+                managementRealm.objects('PermissionChange').filtered('id = $0', permObj.id).addListener((objects, changes) => {
+                    console.log("Permission Status: " + permObj.statusMessage);
+                });
+            }
+            else {
+                console.log("Following error encountered: " + error);
+            }
+        })
+}
+
 
